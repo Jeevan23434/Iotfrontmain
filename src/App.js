24159dafe2d6675a4306d6defc93daeca1193b7b
ramesh <yadavramesh9345@gmail.com>
2024-02-20 23:24:29 +0530

    first push

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import logo from './logo.svg';
+import React, { useEffect, useState, useRef } from "react";
+import axios from 'axios';
+import './App.css';
+
+const host = "http://localhost:5000"
+// const Data=()=>{
+// const [data, setData] = useState([]);
+
+// useEffect(() => {
+//   axios
+//     .get(`${host}/api/patient`)
+//     .then((response) => {
+//       console.log(response.data);
+//       setData(response.data);
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//     });
+// }, []);
+// console.log(data)
+// }
+
+
+
+function App() {
+  const [patdata, patdatachange] = useState(null);
+
+
+  useEffect(() => {
+    fetch(`${host}/data`).then((res) => {
+      return res.json();
+    }).then((resp) => {
+      patdatachange(resp);
+    }).catch((err) => {
+      console.log(err.message);
+    })
+  }, [])
+
+  // const id = data[0].id;
+
+  return (
+    <>
+      <header>
+        <h2>IOT HealthCare Device</h2>
+      </header>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Id. No.</th>
+            <th>Temperature</th>
+            <th>SpO2</th>
+            <th>Pulse Rate</th>
+            <th>Fall Detected</th>
+          </tr>
+        </thead>
+        <tbody>
+
+          {patdata &&
+            patdata.map(item => (
+              <tr key={item.Id}>
+                <td>{item.Id}</td>
+                <td>{item.temperature}</td>
+                <td>{item.spo2}</td>
+                <td>{item.pulse_rate}</td>
+                <td>{item.fall_detected}</td>
+
+              </tr>
+            ))
+          }
+
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+export default App;
